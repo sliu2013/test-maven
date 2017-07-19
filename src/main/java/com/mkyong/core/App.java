e9c0aefb2c9a6338fafc43d826ab897ee2cc3d7d
Sky Liu <sliu@guidewire.com>
2017-07-19 14:27:07 -0700

    second commit

---
+++ src/main/java/com/mkyong/core/App.java
@@ -0,0 +1,24 @@
+package com.mkyong.core;
+
+/**
+ * Created by sliu on 7/18/2017.
+ */
+public class App {
+  public static void main(String[] args) {
+
+    System.out.println(getHelloWorld());
+
+  }
+
+  public static String getHelloWorld() {
+
+    return "Hello World";
+
+  }
+
+  public static String getHelloWorld2() {
+
+    return "Hello World 2";
+
+  }
+}
---
+++ src/test/java/com/mkyong/core/TestApp2.java
@@ -0,0 +1,14 @@
+package com.mkyong.core;
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class TestApp2 {
+
+  @Test
+  public void testPrintHelloWorld2() {
+
+    Assert.assertEquals(App.getHelloWorld2(), "Hello World 2");
+
+  }
+
+}
